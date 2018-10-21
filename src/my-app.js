d3b91e8d962bf805482537f4c5a81230e5171cfe
Basilews <dethroll@gmail.com>
2018-10-21 23:10:57 +0300

    remove redundant views

--- src/my-app.js
+++ src/my-app.js
@@ -31,7 +31,7 @@ setPassiveTouchGestures(true);
 // in `index.html`.
 setRootPath(MyAppGlobals.rootPath);
 
-class MyApp extends PolymerElement {
+class PolymerCalculator extends PolymerElement {
   static get template() {
     return html`
       <style>
@@ -42,34 +42,9 @@ class MyApp extends PolymerElement {
           display: block;
         }
 
-        app-drawer-layout:not([narrow]) [drawer-toggle] {
-          display: none;
-        }
-
         app-header {
           color: #fff;
           background-color: var(--app-primary-color);
-        }
-
-        app-header paper-icon-button {
-          --paper-icon-button-ink-color: white;
-        }
-
-        .drawer-list {
-          margin: 0 20px;
-        }
-
-        .drawer-list a {
-          display: block;
-          padding: 0 16px;
-          text-decoration: none;
-          color: var(--app-secondary-color);
-          line-height: 40px;
-        }
-
-        .drawer-list a.iron-selected {
-          color: black;
-          font-weight: bold;
         }
       </style>
 
@@ -80,32 +55,16 @@ class MyApp extends PolymerElement {
       </app-route>
 
       <app-drawer-layout fullbleed="" narrow="{{narrow}}">
-        <!-- Drawer content -->
-        <app-drawer id="drawer" slot="drawer" swipe-open="[[narrow]]">
-          <app-toolbar>Menu</app-toolbar>
-          <iron-selector selected="[[page]]" attr-for-selected="name" class="drawer-list" role="navigation">
-            <a name="view1" href="[[rootPath]]view1">View One</a>
-            <a name="view2" href="[[rootPath]]view2">View Two</a>
-            <a name="view3" href="[[rootPath]]view3">View Three</a>
-            <a name="new-view" href="[[rootPath]]new-view">New View</a>
-          </iron-selector>
-        </app-drawer>
-
         <!-- Main content -->
         <app-header-layout has-scrolling-region="">
-
           <app-header slot="header" condenses="" reveals="" effects="waterfall">
             <app-toolbar>
-              <paper-icon-button icon="my-icons:menu" drawer-toggle=""></paper-icon-button>
-              <div main-title="">My App</div>
+              <div main-title="">Polymer Calculator</div>
             </app-toolbar>
           </app-header>
 
           <iron-pages selected="[[page]]" attr-for-selected="name" role="main">
             <my-view1 name="view1"></my-view1>
-            <my-view2 name="view2"></my-view2>
-            <my-view3 name="view3"></my-view3>
-            <my-new-view name="new-view"></my-new-view>
             <my-view404 name="view404"></my-view404>
           </iron-pages>
         </app-header-layout>
@@ -138,7 +97,7 @@ class MyApp extends PolymerElement {
      // Show 'view1' in that case. And if the page doesn't exist, show 'view404'.
     if (!page) {
       this.page = 'view1';
-    } else if (['view1', 'view2', 'view3', 'new-view'].indexOf(page) !== -1) {
+    } else if (['view1'].indexOf(page) !== -1) {
       this.page = page;
     } else {
       this.page = 'view404';
@@ -157,22 +116,13 @@ class MyApp extends PolymerElement {
     // statement, so break it up.
     switch (page) {
       case 'view1':
-        import('./my-view1.js');
-        break;
-      case 'view2':
-        import('./my-view2.js');
-        break;
-      case 'view3':
-        import('./my-view3.js');
-        break;
-      case 'new-view':
-        import('./my-new-view.js');
+        import('./view1.js');
         break;
       case 'view404':
-        import('./my-view404.js');
+        import('./view404.js');
         break;
     }
   }
 }
 
-window.customElements.define('my-app', MyApp);
+window.customElements.define('my-app', PolymerCalculator);
