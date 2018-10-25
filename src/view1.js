c4758573e61e0e485346f317bcfd0e2d3f18166e
Basilews <dethroll@gmail.com>
2018-10-25 23:52:01 +0300

    add pad btns

--- src/view1.js
+++ src/view1.js
@@ -19,10 +19,107 @@ class Calculator extends PolymerElement {
           display: block;
           padding: 10px;
         }
+        .pad {
+          max-width: 220px;
+          margin: 0 auto;
+        }
+        .row {
+          display: flex;
+        }
+        .cell {
+          position: relative;
+          width: 48px;
+          height: 48px;
+          display: flex;
+          justify-content: space-between;
+          align-items: center;
+          border: 1px solid black;
+        }
+        .cell:hover {
+          cursor: pointer;
+        }
+        .item {
+          margin: 0 auto;
+        }
       </style>
 
-      <div>
+      <div class="pad">
+        <div class="row">
+          <div class="cell">
+            <span class="item"><</span>
+          </div>
+          <div class="cell">
+            <span class="item">(</span>
+          </div>
+          <div class="cell">
+            <span class="item">)</span>
+          </div>
+          <div class="cell">
+            <span class="item">÷</span>
+          </div>
+        </div>
 
+        <div class="row">
+          <div class="cell">
+            <span class="item">7</span>
+          </div>
+          <div class="cell">
+            <span class="item">8</span>
+          </div>
+          <div class="cell">
+            <span class="item">9</span>
+          </div>
+          <div class="cell">
+            <span class="item">x</span>
+          </div>
+        </div>
+
+        <div class="row">
+          <div class="cell">
+            <span class="item">4</span>
+          </div>
+          <div class="cell">
+            <span class="item">5</span>
+          </div>
+          <div class="cell">
+            <span class="item">6</span>
+          </div>
+          <div class="cell">
+            <span class="item">-</span>
+          </div>
+        </div>
+
+        <div class="row">
+          <div class="cell">
+            <span class="item">1</span>
+          </div>
+          <div class="cell">
+            <span class="item">2</span>
+          </div>
+          <div class="cell">
+            <span class="item">3</span>
+          </div>
+          <div class="cell">
+            <span class="item">+</span>
+          </div>
+        </div>
+
+        <div class="row">
+          <div class="cell">
+            <span class="item">0</span>
+          </div>
+          <div class="cell">
+            <span class="item">.</span>
+          </div>
+          <div class="cell">
+            <div class="cell-plus">+</div>
+            <div class="cell-slash">/</div>
+            <div class="cell-minus">-</div>
+          </div>
+          <div class="cell">
+            <span class="item">=</span>
+          </div>
+        </div>
       </div>
     `;
   }
